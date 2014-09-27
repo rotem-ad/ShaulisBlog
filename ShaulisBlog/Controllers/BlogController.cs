d77f64d0243dafb43ba0286fd9851c7ca169dc11
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-09-27 23:36:41 +0300

    Add blog guests functionality

--- ShaulisBlog/Controllers/BlogController.cs
+++ ShaulisBlog/Controllers/BlogController.cs
@@ -1,20 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShaulisBlog.Models;
+using ShaulisBlog.DAL;
 
 namespace ShaulisBlog.Controllers
 {
     public class BlogController : Controller
     {
+        private BlogDBContext db = new BlogDBContext();
+
         //
         // GET: /Blog/
 
         public ActionResult Index()
         {
+            return View(db.Posts.ToList());
+        }
+
+        //
+        // GET: /Blog/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Blog/Create
+
+        public ActionResult Create()
+        {
             return View();
         }
 
+        //
+        // POST: /Blog/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        //
+        // POST: /Blog/CreateComment
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateComment(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comment);
+        }
+
+        //
+        // GET: /Blog/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Blog/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Blog/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Blog/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
