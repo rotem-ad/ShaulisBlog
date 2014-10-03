d92447e3bb7b08e06bb0dab940e6714af5f67020
Miri Kuskina <miri86kn@gmail.com>
2014-10-03 19:49:09 +0300

    Statistics page - final, fix paths, queries & ui

--- ShaulisBlog/Scripts/Statistics.js
+++ ShaulisBlog/Scripts/Statistics.js
@@ -1,7 +1,11 @@
 ﻿
+var svg;
 
 
 var Statistics = {
+    //flag to determine if graph has already been generated
+    HaveGraph: new Array(),
+
     //turn on or off relevan statistics details
     ShowHideDetails: function (itemId) {
         var detailsId = '#' +itemId + 'Result'; //calc id of result div
@@ -9,54 +13,64 @@ var Statistics = {
             $(detailsId).hide();
         else {
             $(detailsId).show();
+            Statistics.DisplayGraph(itemId + 'Result');
+        }
+    },
 
-         
-
-            d3.tsv("postsPerMonth.tsv", type, function (error, data) {
-                x.domain(data.map(function (d) { return d.Month; }));
-                y.domain([0, d3.max(data, function (d) { return d.PostsCount; })]);
-
-                svg.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis);
-
-                svg.append("g")
-                    .attr("class", "y axis")
-                    .call(yAxis)
-                  .append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em")
-                    .style("text-anchor", "end")
-                    .text("PostsCount");
-
-                svg.selectAll(".bar")
-                    .data(data)
-                  .enter().append("rect")
-                    .attr("class", "bar")
-                    .attr("x", function (d) { return x(d.Month); })
-                    .attr("width", x.rangeBand())
-                    .attr("y", function (d) { return y(d.PostsCount); })
-                    .attr("height", function (d) { return height - y(d.PostsCount); });
-
-            });
+    //generate graph to show results
+    DisplayGraph: function (reportName) {
+        if (Statistics.HaveGraph == null || Statistics.HaveGraph[reportName] == null) {
+            svg = d3.select('#' + reportName).append("svg")
+                       .attr("width", width + margin.left + margin.right)
+                       .attr("height", height + margin.top + margin.bottom)
+                       .append("g")
+                       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+            d3.tsv(reportName + ".tsv",
+                function (d) {
+                    d.Count = +d.Count;
+                    return d;
+                },
+                function (error, data) {
+                    x.domain(data.map(function (d) { return d.Item; }));
+                    y.domain([0, d3.max(data, function (d) { return d.Count; })]);
 
+                    svg.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
 
+                    svg.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis)
+                      .append("text")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y", 6)
+                        .attr("dy", ".71em")
+                        .style("text-anchor", "end")
+                        .text("Count"); //CHANGE!!!!!!!!!!
+
+                    svg.selectAll(".bar")
+                        .data(data)
+                      .enter().append("rect")
+                        .attr("class", "bar")
+                        .attr("x", function (d) { return x(d.Item); })
+                        .attr("width", x.rangeBand())
+                        .attr("y", function (d) { return y(d.Count); })
+                        .attr("height", function (d) { return height - y(d.Count); });
+
+                });
+
+            Statistics.HaveGraph[reportName] = true;
         }
     }
  }
 
 
-
-
-
-
  var margin = {top: 20, right: 20, bottom: 30, left: 40},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+            width = 600 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
 
         var x = d3.scale.ordinal()
             .rangeRoundBands([0, width], .1);
@@ -71,16 +85,6 @@ var Statistics = {
         var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left")
-            .ticks(10, "%");
+            .ticks(10, "");
+       
 
-        var svg = d3.select("body").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
-function type(d) {
-    d.frequency = +d.frequency;
-    return d;
-}
