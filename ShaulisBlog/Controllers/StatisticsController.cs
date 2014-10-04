220b9fc01fb504935012c24e2db65617fdf3d78a
miri86kn <1q2w3e4r>
2014-10-04 15:19:06 +0300

    Moved statistics logic to "Model"

--- ShaulisBlog/Controllers/StatisticsController.cs
+++ ShaulisBlog/Controllers/StatisticsController.cs
@@ -1,4 +1,5 @@
 ﻿using ShaulisBlog.DAL;
+using ShaulisBlog.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,8 @@ namespace ShaulisBlog.Controllers
     public class StatisticsController : Controller
     {
 
-        private BlogDBContext db = new BlogDBContext();
+        Statistics statistics = new Statistics();
+
 
         //
         // GET: /Statistics/
@@ -18,60 +20,12 @@ namespace ShaulisBlog.Controllers
         public ActionResult Index()
         {
             //generate statistic files
-            GenerateStatisticFiles();
+            statistics.GenerateStatisticFiles();
 
             return View();
         }
 
-        private void GenerateStatisticFiles() {
-            //How many posts were added per month?    -- get the data
-            var postsPerMonth = db.Posts.GroupBy(x => new { x.PublishDate.Year, x.PublishDate.Month }).Select(g => new { Key = g.Key, Count = g.Count() }).OrderBy(x => x.Key.Year).ThenBy(x => x.Key.Month);
-            string path = Server.MapPath("ShowPostsPerMonthResult.tsv");
-            
-            //write to file
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, false))
-            {
-               string line = string.Format("{0}\t{1}", "Item", "Count"); //do headline
-               file.WriteLine(line);
-
-               foreach (var data in postsPerMonth) {
-                   line = string.Format("{0}/{1}\t{2}", data.Key.Month, data.Key.Year, data.Count); 
-                   file.WriteLine(line);
-               }
-            }
-
-            //How many comments were added per month?   --get the data
-            var commentsPerMonth = db.Comments.GroupBy(x => new { x.Post.PublishDate.Year, x.Post.PublishDate.Month }).Select(g => new { Key = g.Key, Count = g.Count() }).OrderBy(x => x.Key.Year).ThenBy(x => x.Key.Month);
-            path = Server.MapPath("ShowCommentsPerMonthResult.tsv");
-            //write to file
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, false))
-            {
-                string line = string.Format("{0}\t{1}", "Item", "Count"); //do headline
-                file.WriteLine(line);
-
-                foreach (var data in commentsPerMonth)
-                {
-                    line = string.Format("{0}/{1}\t{2}", data.Key.Month, data.Key.Year, data.Count);
-                    file.WriteLine(line);
-                }
-            }
-
-            //How many fans do we have?
-            var fansCount = db.Fans.GroupBy(x=> x.Seniority).Select(g=> new {Key = g.Key, Count = g.Count()}).OrderBy(x=>x.Key);
-            path = Server.MapPath("ShowFansResult.tsv");
-            //write to file
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, false))
-            {
-                string line = string.Format("{0}\t{1}", "Item", "Count"); //do headline
-                file.WriteLine(line);
-
-                foreach (var data in fansCount)
-                {
-                    line = string.Format("Seniority:{0}\t{1}", data.Key, data.Count); 
-                    file.WriteLine(line);
-                }
-            }
-        }
+       
 
     }
 
---
+++ ShaulisBlog/Models/Statistics.cs
@@ -0,0 +1,65 @@
+﻿using ShaulisBlog.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShaulisBlog.Models
+{
+    public class Statistics
+    {
+        private BlogDBContext db = new BlogDBContext();
+
+        public void GenerateStatisticFiles()
+        {
+            //How many posts were added per month?    -- get the data
+            var postsPerMonth = db.Posts.GroupBy(x => new { x.PublishDate.Year, x.PublishDate.Month }).Select(g => new { Key = g.Key, Count = g.Count() }).OrderBy(x => x.Key.Year).ThenBy(x => x.Key.Month);
+            string path = System.Web.HttpContext.Current.Server.MapPath("ShowPostsPerMonthResult.tsv");
+
+            //write to file
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, false))
+            {
+                string line = string.Format("{0}\t{1}", "Item", "Count"); //do headline
+                file.WriteLine(line);
+
+                foreach (var data in postsPerMonth)
+                {
+                    line = string.Format("{0}/{1}\t{2}", data.Key.Month, data.Key.Year, data.Count);
+                    file.WriteLine(line);
+                }
+            }
+
+            //How many comments were added per month?   --get the data
+            var commentsPerMonth = db.Comments.GroupBy(x => new { x.Post.PublishDate.Year, x.Post.PublishDate.Month }).Select(g => new { Key = g.Key, Count = g.Count() }).OrderBy(x => x.Key.Year).ThenBy(x => x.Key.Month);
+            path = System.Web.HttpContext.Current.Server.MapPath("ShowCommentsPerMonthResult.tsv");
+            //write to file
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, false))
+            {
+                string line = string.Format("{0}\t{1}", "Item", "Count"); //do headline
+                file.WriteLine(line);
+
+                foreach (var data in commentsPerMonth)
+                {
+                    line = string.Format("{0}/{1}\t{2}", data.Key.Month, data.Key.Year, data.Count);
+                    file.WriteLine(line);
+                }
+            }
+
+            //How many fans do we have?
+            var fansCount = db.Fans.GroupBy(x => x.Seniority).Select(g => new { Key = g.Key, Count = g.Count() }).OrderBy(x => x.Key);
+            path = System.Web.HttpContext.Current.Server.MapPath("ShowFansResult.tsv");
+            //write to file
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, false))
+            {
+                string line = string.Format("{0}\t{1}", "Item", "Count"); //do headline
+                file.WriteLine(line);
+
+                foreach (var data in fansCount)
+                {
+                    line = string.Format("Seniority:{0}\t{1}", data.Key, data.Count);
+                    file.WriteLine(line);
+                }
+            }
+        }
+    }
+}
